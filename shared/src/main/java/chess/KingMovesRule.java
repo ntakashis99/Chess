4c2828b4a66b01b4cbae993ca88020248ff2d50b
ntakashis99 <nephi.suyama@gmail.com>
2024-09-21 12:59:45 -0600

    Brought the check moves class to the abstract class.

--- shared/src/main/java/chess/KingMovesRule.java
+++ shared/src/main/java/chess/KingMovesRule.java
@@ -7,26 +7,7 @@ import java.util.List;
 public class KingMovesRule extends PieceMoveRule {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> moves = new ArrayList<>();
         int[][] move_changes = new int[][]{{1, -1}, {0,-1}, {0,1}, {-1,0}, {1,0}, {-1, 1}, {-1, -1}, {1, 1}};
-        for (int[] move : move_changes) {
-            //Add boundary checking for positions
-            if((myPosition.getRow() + move[0] > 8) ||(myPosition.getRow() + move[0] < 1 ) || (myPosition.getColumn()+ move[1] > 8) || (myPosition.getColumn()+ move[1] < 1)){
-                break;
-            }
-            else {
-                ChessPosition newPosition = new ChessPosition(myPosition.getRow() + move[0], myPosition.getColumn() + move[1]);
-                ChessPiece piece = board.getPiece(newPosition);
-                if (piece != null) {
-                    if (piece.getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                        moves.add(new ChessMove(myPosition, newPosition, null));
-                    }
-                }
-                else {
-                    moves.add(new ChessMove(myPosition, newPosition, null));
-                }
-            }
-        }
-        return moves;
+        return check_moves(board,myPosition,move_changes);
     }
 }
--- shared/src/main/java/chess/PieceMoveRule.java
+++ shared/src/main/java/chess/PieceMoveRule.java
@@ -1,9 +1,32 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public abstract class PieceMoveRule {
     public abstract Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition);
-
+    public Collection<ChessMove> check_moves(ChessBoard board, ChessPosition myPosition, int[][] move_changes){
+        Collection<ChessMove> moves = new ArrayList<>();
+        for (int[] move : move_changes) {
+            //Add boundary checking for positions
+            if((myPosition.getRow() + move[0] > 8) ||(myPosition.getRow() + move[0] < 1 ) || (myPosition.getColumn()+ move[1] > 8) || (myPosition.getColumn()+ move[1] < 1)){
+                break;
+            }
+            else {
+                ChessPosition newPosition = new ChessPosition(myPosition.getRow() + move[0], myPosition.getColumn() + move[1]);
+                ChessPiece piece = board.getPiece(newPosition);
+                if (piece != null) {
+                    if (piece.getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                        moves.add(new ChessMove(myPosition, newPosition, null));
+                    }
+                }
+                else {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                }
+            }
+        }
+        return moves;
+    }
     //public Collection<ChessMove> checkDiag();
+
 }
