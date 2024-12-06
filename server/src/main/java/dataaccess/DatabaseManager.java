5e982fefde8231db1dfa4a9f547200a531ac6823
ntakashis99 <nephi.suyama@gmail.com>
2024-12-05 21:41:03 -0700

    Added createStatements

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -70,13 +70,55 @@ public class DatabaseManager {
         }
     }
 
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS AuthData (
+            `username` varchar(256) NOT NULL,
+            `authToken` varchar(256) NOT NULL,
+            PRIMARY KEY (`username`)
+            INDEX(authToken)
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS UserData (
+            `username` varchar(256) NOT NULL,
+            `password` varchar(256) NOT NULL,
+            `email` varchar(256) NOT NULL,
+            PRIMARY KEY (`username`)
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS GameData (
+            `gameID` int NOT NULL,
+            `whiteUsername` varchar(256),
+            `blackUsername` varchar(256),
+            `gameName` varchar(256) NOT NULL,
+            `game` text NOT NULL,
+            PRIMARY KEY (`username`),
+            INDEX(authToken)
+            )
+            """
+    };
+
     //ADD code to initialize the tables (CREATE TABLE IF NOT EXIST) check petshop
     public static void createTables() throws DataAccessException {
         try {
-            var statement = "CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME;
+            var statement = "CREATE TABLE IF NOT EXISTS " + "AuthData";
+
             var conn = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
             try (var preparedStatement = conn.prepareStatement(statement)) {
                 preparedStatement.executeUpdate();
+            }
+            var statement1 = "CREATE TABLE IF NOT EXISTS " + "UserData";
+            var conn1 = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
+            try (var preparedStatement1 = conn1.prepareStatement(statement1)) {
+                preparedStatement1.executeUpdate();
+            }
+            var statement2 = "CREATE TABLE IF NOT EXISTS " + "GameData";
+            var conn2 = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
+            try (var preparedStatement2 = conn2.prepareStatement(statement2)) {
+                preparedStatement2.executeUpdate();
             }
         } catch (SQLException e) {
             throw new DataAccessException(e.getMessage());
